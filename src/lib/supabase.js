dec4a9be8a2adc4b72c487f342abcccec9124960
phancongvuduc <duc.phan@arentvn.com>
2025-04-26 16:02:04 +0700

    fix logic supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,10 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-// Lấy giá trị từ biến môi trường hoặc sử dụng giá trị cứng trong trường hợp biến môi trường không hoạt động
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://kecyufofrwkqftmgswee.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlY3l1Zm9mcndrcWZ0bWdzd2VlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NDgzNDgsImV4cCI6MjA2MTIyNDM0OH0.OjXkpfvlvNKOWGUNleisgSAGtv1UfZypeFEN4MM3lSg';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-console.log('Supabase URL being used:', supabaseUrl);
-
-// Khởi tạo Supabase client
-export const supabase = createClient(supabaseUrl, supabaseKey); 
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
